ee8fa5d973924c98f4df44a94087d19baf28b8ca
George Mount <gxm204@case.edu>
2021-05-05 14:50:09 -0400

    update files

--- george-udfs-test/myproject.py
+++ george-udfs-test/myproject.py
@@ -6,10 +6,6 @@ import re
 # https://stackoverflow.com/questions/11243423/getting-email-address-from-a-cell-in-excel
 # Python https://www.tutorialspoint.com/python_text_processing/python_extract_emails_from_text.htm  
 
-@xw.func
-def find_emails(str):
-    adds = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", str)
-    return adds
 
 
 
@@ -31,8 +27,23 @@ if __name__ == "__main__":
     xw.Book("myproject.xlsm").set_mock_caller()
     main()
 
+########
+
 
 @xw.func
-@xw.arg("df", pd.DataFrame, index=True, header=True)
-def describe(df):
+def py_find_emails(str):
+    adds = re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.[a-z]+", str)
+    return adds
+
+
+
+@xw.func
+@xw.arg("df", pd.DataFrame, index=False, header=True)
+def py_describe(df):
     return df.describe()
+
+
+@xw.func
+@xw.arg("df", pd.DataFrame, index=False, header=True)
+def py_corr(df):
+    return df.corr()
